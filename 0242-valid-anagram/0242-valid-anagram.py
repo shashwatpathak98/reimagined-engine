c8651304f0dec770e52e0c41baa99296df9f9aa1
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2023-10-15 23:38:14 +0530

    Time: 55 ms (53.71%), Space: 17.6 MB (10.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hashmap = {}
-        hashmap2 = {}
-        for i in range(len(s)):
-            if  s[i] not in hashmap:
-                hashmap[s[i]] = 1
-            else:
-                hashmap[s[i]] +=  1
+        return sorted(s) == sorted(t)
+        # hashmap = {}
+        # hashmap2 = {}
+        # for i in range(len(s)):
+        #     if  s[i] not in hashmap:
+        #         hashmap[s[i]] = 1
+        #     else:
+        #         hashmap[s[i]] +=  1
                
-        for j in range(len(t)):
-            if  t[j] not in hashmap2:
-                hashmap2[t[j]] = 1
-            else:
-                hashmap2[t[j]] +=  1
-        # print(hashmap)        
-        # print(hashmap2)
-        if hashmap == hashmap2:
-            return True
-        return False             
+        # for j in range(len(t)):
+        #     if  t[j] not in hashmap2:
+        #         hashmap2[t[j]] = 1
+        #     else:
+        #         hashmap2[t[j]] +=  1
+        # # print(hashmap)        
+        # # print(hashmap2)
+        # if hashmap == hashmap2:
+        #     return True
+        # return False             
                   
             
