6082b84037661bf0e2df418e92e930280a3e8812
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2023-10-15 21:07:26 +0530

    Time: 468 ms (80.6%), Space: 34.3 MB (7.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashmap = {} 
+        hashmap = {}
+        value = 1
         for i in range(len(nums)):
             key = nums[i]
             if key in hashmap: 
                 return True            
-            hashmap[key] = 1     
+            hashmap[key] = value     
         return False
                
                
