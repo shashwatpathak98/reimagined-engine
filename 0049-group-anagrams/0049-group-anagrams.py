4034b2bb13be65db778ec32bd9b8ade69cf6fae7
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2023-10-16 16:45:25 +0530

    Time: 91 ms (87.9%), Space: 19.5 MB (89.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dictionary = {}
+        dictionary = defaultdict(list)
         for string in strs:
             # first we have to sort each element
             key = "".join(sorted(list(string)))
-            if(key not in dictionary):
-                dictionary[key] = [string]
-            else:
-                dictionary[key].append(string)
+            dictionary[key].append(string)
         arr = list(dictionary.values())
         return arr
 
