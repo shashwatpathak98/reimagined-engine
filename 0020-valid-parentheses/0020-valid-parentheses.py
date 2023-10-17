1a21939b42ae909ea592cae163752bbe926d1e52
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2023-10-18 01:20:04 +0530

    Time: 37 ms (69.09%), Space: 16.3 MB (66.23%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,13 +1,13 @@
 class Solution:
     def isValid(self, string: str) -> bool:
         stack = []
-        dictionary = {')': '(', ']': '[', '}': '{'}      
+        dictionary = {')': '(', ']': '[', '}': '{'}    
         for character in string:
             lengthOfStack = len(stack)
             if character in dictionary:
                 lastcharacter = stack.append(" ") if lengthOfStack is 0 else stack.pop()
-                if dictionary[character] is not lastcharacter:  
-                    return False       
+                if dictionary[character] is not lastcharacter: 
+                    return False  
             else:
                 stack.append(character)  
                 
