7dfa25bab3284cf40395db36057dc95142a387c6
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2023-10-18 01:40:21 +0530

    Time: 45 ms (17.35%), Space: 16.1 MB (92.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,7 +4,7 @@ class Solution:
         dictionary = {')': '(', ']': '[', '}': '{'}    
         for character in string: 
             if character in dictionary:
-                lastcharacter = stack.pop() if length_hint(stack) != 0  else " "  
+                lastcharacter = stack.pop() if len(stack) != 0  else " "  
                 if dictionary[character] != lastcharacter: return False
             else:
                 stack.append(character)  
