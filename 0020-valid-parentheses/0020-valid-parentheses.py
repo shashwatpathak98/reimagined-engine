264f2c512a6c5f68fee6040c2f58edcc945a07f6
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2023-10-18 01:25:15 +0530

    Time: 37 ms (69.09%), Space: 16.3 MB (66.23%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -6,8 +6,7 @@ class Solution:
             lengthOfStack = len(stack)
             if character in dictionary:
                 lastcharacter = stack.append(" ") if lengthOfStack is 0 else stack.pop()
-                if dictionary[character] is not lastcharacter: 
-                    return False  
+                if dictionary[character] is not lastcharacter: return False  
             else:
                 stack.append(character)  
                 
