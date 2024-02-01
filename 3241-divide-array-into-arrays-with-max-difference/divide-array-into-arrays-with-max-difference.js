d42b03e634f4fe095b0958255a0064ab8207f876
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2024-02-02 01:26:45 +0530

    Time: 360 ms (23.82%) | Memory: 78.1 MB (98.43%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+    nums.sort((a,b) => a-b)
+    let ans = [];
+
+    for(let i= 0 ; i < nums.length ; i+=3) {
+         if(nums[i+2] - nums[i] > k){
+             return [];
+         }
+         ans.push([nums[i] ,nums[i+1] , nums[i+2]]);
+    }
+
+    return ans;
+
+};
