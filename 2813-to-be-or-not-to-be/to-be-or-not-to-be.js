5c810683407134cd007105f38e860007828a582f
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2024-11-13 00:03:05 +0530

    Time: 42 ms (96.25%) | Memory: 48.9 MB (50.14%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+const expect = (val) => {
+
+
+    return {
+        toBe: function (expected) {
+
+            if (val === expected) {
+                return true
+            }
+            else {
+                throw new Error("Not Equal")
+            }
+        }
+        ,
+
+
+        notToBe: function (expected) {
+
+            if (val !== expected) {
+                return true
+            }
+
+
+            throw new Error("Equal")
+
+        }
+
+
+    }
+
+
+
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
