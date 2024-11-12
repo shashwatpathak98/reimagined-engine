0b3b341f002da8e6fec1b88c7ec6c5e2fb1a33a5
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2024-11-13 00:17:14 +0530

    Time: 46 ms (87.66%) | Memory: 48.5 MB (81.60%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -4,38 +4,21 @@
  */
 const expect = (val) => {
 
-
     return {
-        toBe: function (expected) {
-
-            if (val === expected) {
+        toBe: (expected) => {
+            if (val === expected){
                 return true
             }
-            else {
-                throw new Error("Not Equal")
-            }
-        }
-        ,
-
-
-        notToBe: function (expected) {
-
-            if (val !== expected) {
+            throw new Error("Not Equal")
+         },
+        notToBe: (expected) => {
+             if (val !== expected){
                 return true
             }
-
-
             throw new Error("Equal")
-
         }
-
-
     }
-
-
-
 };
-
 /**
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
