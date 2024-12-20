f99eb380a4cb85714210b7fdc23ace43e36241e6
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2024-12-20 14:14:54 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (9.08%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -7,7 +7,7 @@ class Solution:
             mid = (l + r) // 2
             if target == nums[mid]:
                 return mid
-            elif target <= nums[mid]:
+            elif target < nums[mid]:
                 r = mid - 1
             else:
                 l = mid + 1
