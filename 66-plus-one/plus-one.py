a48364fc30aa9e58c1b0345efd130eeb1370c733
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2024-12-26 20:39:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (6.18%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,26 +1,40 @@
-import numpy as np
-
 class Solution:
+
     def plusOne(self, digits: List[int]) -> List[int]:
+        # carry = 0
+        # for index in range(len(digits) - 1, -1, -1):
+        #     if index == len(digits) - 1:
+        #         digits[index] += 1
+        #         if digits[index] > 9:
+        #             digits[index] = 0
+        #             carry = 1
+        #     else:
+        #         if carry == 1:
+        #             digits[index] += 1
+        #             if digits[index] > 9:
+        #                 digits[index] = 0
+        #                 carry = 1
+        #             else:
+        #                 carry = 0
+
+        # return digits if carry == 0 else [carry] + digits
         carry = 0
-        for index in range(len(digits)-1 , -1, -1):
-            if index == len(digits) - 1:
-                digits[index]+=1
-                if digits[index] > 9:
-                    digits[index] = 0
+
+        for i in range(len(digits) - 1, -1, -1):
+            if i == len(digits) - 1:
+                digits[i] += 1
+
+                if digits[i] > 9:
                     carry = 1
+                    digits[i] = 0
+
             else:
                 if carry == 1:
-                    digits[index]+= 1
-                    if digits[index] > 9:
-                        digits[index] = 0
+                    digits[i] += 1
+                    
+                    if digits[i] > 9:
                         carry = 1
+                        digits[i] = 0
                     else:
-                        carry = 0    
-        
-        return digits if carry == 0 else [carry]+digits
-        
-        
-
-
-        
+                        carry = 0
+        return digits if carry == 0 else [carry]+digits                 
