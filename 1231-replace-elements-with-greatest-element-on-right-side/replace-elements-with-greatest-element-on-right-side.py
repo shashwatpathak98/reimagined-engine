60316d874708f28d3878f29b022890646bd1da49
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2025-02-06 01:33:43 +0530

    Time: 35 ms (69.82%) | Memory: 18.7 MB (46.36%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -7,9 +7,7 @@ class Solution:
         right_max = -1
 
         for i in range(len(arr)-1 , -1 , -1):
-            new_max = max(right_max , arr[i])
-            arr[i] = right_max
-            right_max = new_max
+            arr[i] , right_max  = right_max, max(right_max , arr[i])
         return arr    
 
 
