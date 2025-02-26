0c6137d6aae8c30154a9bce3422800d33eb52411
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2025-02-26 20:47:16 +0530

    Time: 54 ms (82.95%) | Memory: 17.8 MB (87.52%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -5,12 +5,16 @@ class Solution:
             nums[i] = nums[i] | nums[i+n]
 
         j = 2*n - 1
-        
-        for i in range(n-1 , -1 , -1):
+
+        for i in range(n-1, -1, -1):
             y = nums[i] & (2**10 - 1)
             x = nums[i] >> 10
 
             nums[j] = y
             nums[j-1] = x
             j-=2
-        return nums    
+
+        return nums
+
+
+        
