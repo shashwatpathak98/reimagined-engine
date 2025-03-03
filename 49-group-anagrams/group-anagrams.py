947b678aeded287ba5c43c0c2275127142e48158
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2025-03-04 00:05:01 +0530

    Time: 11 ms (83.57%) | Memory: 20.3 MB (99.40%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,26 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
         hash_map = {}
-        
-
+       
         for s in strs:
-            key = "".join(sorted(s))
-            
-            #print(hash_map)
-            if(key in hash_map):
-                hash_map[key].append(s)
+            sorted_s = "".join(sorted(s))
+            if sorted_s in hash_map:
+                hash_map[sorted_s].append(s)
             else:
-                hash_map[key] = [s]
-
+                hash_map[sorted_s] = [s]
         return list(hash_map.values())
-             
 
 
-        
 
-            
-        
-
-             
         
