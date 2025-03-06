a6d63fc78793c830f1d0ad722f8e63dcb0b0e0e8
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2025-03-07 01:51:55 +0530

    Time: 8 ms (58.29%) | Memory: 17.9 MB (97.40%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,18 +1,14 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        combined_grid = []
         n = len(grid)
-        for i in range(n):
-            for j in range(n):
-                combined_grid.append(grid[i][j])
-        print(combined_grid)
-
         hash_map = {}
-        for i in combined_grid:
-            if i not in hash_map:
-                hash_map[i] = 1
-            else:
-                hash_map[i] += 1
+
+        for row in grid:
+            for i in row:
+                if i not in hash_map:
+                    hash_map[i] = 1
+                else:
+                    hash_map[i] += 1
 
         missing, repeated = -1, -1
         for i in range(1, n * n + 1):
