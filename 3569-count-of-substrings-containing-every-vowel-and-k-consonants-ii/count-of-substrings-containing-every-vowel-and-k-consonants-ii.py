804cb3a1bc17ea19256c2e46e9a322925841f876
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2025-03-12 01:51:09 +0530

    Time: 1804 ms (57.40%) | Memory: 18.8 MB (30.99%) - LeetSync

---
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+class Solution:
+    def _isVowel(self , c: str) -> bool:
+        return c in 'aeiou'
+
+    def _atLeastK(self, word:str, k:int) -> int:
+        count, start, vowels, consonants = 0,0,{},0
+
+        for end, char in enumerate(word):
+            if self._isVowel(char):
+                vowels[char] = vowels.get(char , 0) + 1
+            else:
+                consonants += 1
+        
+            while len(vowels) == 5 and consonants >= k:
+                count += len(word) - end
+
+                if self._isVowel(word[start]):
+                    vowels[word[start]] -= 1
+                    if vowels[word[start]] == 0:
+                        del vowels[word[start]]
+                else:
+                    consonants -= 1
+
+                start += 1
+        return count                     
+
+
+    def countOfSubstrings(self, word: str, k: int) -> int:
+       return self._atLeastK(word, k) -  self._atLeastK(word, k+1)
