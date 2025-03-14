bd7f11c789a3d3b1ecc5c170af7adaa4666e43f9
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2025-03-15 03:17:22 +0530

    Time: 699 ms (47.47%) | Memory: 63.1 MB (77.31%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -10,26 +10,26 @@ class Solution:
         while left <= right:
             middle = left + (right - left) // 2
             if self.can_form_zero_array(nums, queries, middle):
-                    right = middle - 1
-            else:   
-                    left = middle + 1
-        return left
+                right = middle - 1
+            else:
+                left = middle + 1
+        return left                
 
 
-    def can_form_zero_array(self, nums: List[int], queries: List[List[int]], k: int) -> bool:
+    def can_form_zero_array(self , nums: List[int], queries: List[List[int]] , k:int):
         n = len(nums)
         total_sum = 0
         difference_array = [0] * (n+1)
 
         for query_index in range(k):
             start, end, val = queries[query_index]
-            
+
             difference_array[start] += val
-            difference_array[end + 1] -= val
+            difference_array[end+1] -= val    
 
         for num_index in range(n):
             total_sum += difference_array[num_index]
-            if total_sum  <  nums[num_index]:
+            if total_sum < nums[num_index]:
                 return False
         return True
 
@@ -37,8 +37,4 @@ class Solution:
 
 
 
-
-
-
-        
         
