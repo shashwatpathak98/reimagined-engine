a30c6d06d2b948e50ec7cdb1639281e7dfb5a5db
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2025-03-16 17:01:58 +0530

    Time: 580 ms (60.59%) | Memory: 64.6 MB (5.05%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -1,49 +1,47 @@
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
 
-        n = len(nums)
-        left , right = 0 , len(queries)
+        # length of nums array , start index and end index of queries array
+        n, start, end = len(nums), 0, len(queries)
 
-        if not self.can_form_zero_array(nums, queries, right):
+        # If zero array cannot be formed return -1
+        if not self.can_form_zero_array(nums, queries, end):
             return -1
 
-        while left <= right:
-            mid = (left + right) // 2
-            if self.can_form_zero_array(nums, queries , mid):
-                right = mid -1
+        # Binary search to find minimum range at which nums will be zero
+        while start <= end:
+            mid = (start + end) // 2
+            if self.can_form_zero_array(nums, queries, mid):
+                end = mid - 1
             else:
-                left = mid + 1    
-        return left    
+                start = mid + 1
+        return start
 
-
-
-    def can_form_zero_array(self, nums: list[int] , queries:List[List[int]], k:int) -> bool:
-        
+    # Helper Function to check if zero array can be formed
+    def can_form_zero_array(self, nums: List[int], queries: List[List[int]], k: int) -> bool:
         n = len(nums)
-        total_sum = 0
-        difference_array = [0] * (n+1)
+        # Differnce array - to include range of elements
+        difference_array = [0] * (n + 1)
+        # prefix_sum represents the total value applied from the queries up to the current index i
+        prefix_sum = 0
 
-        for q in range(k):
-            start , end, value = queries[q]
+        # Fill difference array
+        for i in range(k):
 
-            difference_array[start] += value
-            difference_array[end+1] -= value
+            # get start, end and values from queries
+            start, end, val = queries[i]
 
+            # Calculate the difference array
+            difference_array[start] += val
+            difference_array[end + 1] -= val
+
+        # Check if the prefix_sum of difference array is less than any of the nums element,
+        # if yes that means it cannot form zero array, so return False
         for i in range(n):
-            total_sum += difference_array[i]
-            if total_sum < nums[i]:
+            prefix_sum += difference_array[i]
+            # if the prefix_sum is less than nums[i], it means the required value at that index can't be reached using the queries processed so far.
+            # Hence,return False
+            if prefix_sum < nums[i]:
                 return False
-        return True        
-
-
-
-
-
-
-
-            
-
-        
-
-
-        
+        # all indices in nums can be satisfied, and it returns True.
+        return True
