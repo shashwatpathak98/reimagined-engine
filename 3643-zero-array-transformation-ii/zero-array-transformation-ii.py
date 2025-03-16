84326776a58a89080e88f860251f7784f197a6ef
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2025-03-16 11:34:15 +0530

    Time: 599 ms (59.57%) | Memory: 64.6 MB (5.05%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -2,37 +2,48 @@ class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
 
         n = len(nums)
-        left, right = 0 , len(queries)
+        left , right = 0 , len(queries)
 
         if not self.can_form_zero_array(nums, queries, right):
             return -1
-        
+
         while left <= right:
-            middle = left + (right - left) // 2
-            if self.can_form_zero_array(nums, queries, middle):
-                right = middle - 1
+            mid = (left + right) // 2
+            if self.can_form_zero_array(nums, queries , mid):
+                right = mid -1
             else:
-                left = middle + 1    
-        return left
+                left = mid + 1    
+        return left    
 
-    def can_form_zero_array(self, nums: List[int], queries: List[List[int]], k:int) -> bool:
 
+
+    def can_form_zero_array(self, nums: list[int] , queries:List[List[int]], k:int) -> bool:
+        
         n = len(nums)
         total_sum = 0
         difference_array = [0] * (n+1)
 
-        for query_index in range(k):
-            start, end, value = queries[query_index]
+        for q in range(k):
+            start , end, value = queries[q]
 
             difference_array[start] += value
             difference_array[end+1] -= value
 
-        for num_index in range(n):
-            total_sum += difference_array[num_index]
-            if total_sum < nums[num_index]:
+        for i in range(n):
+            total_sum += difference_array[i]
+            if total_sum < nums[i]:
                 return False
         return True        
 
+
+
+
+
+
+
+            
+
+        
 
 
         
