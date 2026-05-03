3c2858d192479b8ec1530eacb2dad1335f43e274
Shashwat Pathak <111122076+shashwatpathak98@users.noreply.github.com>
2026-05-03 17:21:42 +0530

    Time: 4 ms (38.87%) | Memory: 20.8 MB (5.39%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,29 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #Brute Force
-        n = len(nums)
-        for i in range(n):
-            for j in range(i+1 , n):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        return []
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1 , n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+        # return []
+
+        # Two Pointers
+        nums_with_index = sorted([[num, i] for i, num in enumerate(nums)])
+        n = len(nums_with_index) - 1
+        l,r = 0,n
+        while l<r:
+            sum = nums_with_index[l][0] + nums_with_index[r][0]
+            if sum == target:
+                return [nums_with_index[l][1],nums_with_index[r][1]]
+            elif sum < target:
+                l+=1
+            else:
+                r-=1
+        return []        
+                      
+
+
 
 
         
